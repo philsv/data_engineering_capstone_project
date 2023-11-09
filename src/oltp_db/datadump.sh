064f90e4dca6ec7e72b33d89f0e6d2350654221e
philsv <35413649+philsv@users.noreply.github.com>
2023-11-09 17:39:24 +0100

    Added new files and scripts for OLTP database setup and data manipulation

---
+++ src/oltp_db/datadump.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mysqldump -u root -p sales sales_data > sales_data.sql
---
+++ src/oltp_db/db_setup.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE sales;
+USE sales;
+
+CREATE TABLE sales_data(
+    product_id INT ,
+    customer_id INT NOT NULL PRIMARY KEY,
+    price INT,
+    quantity INT,
+    timestamp DATETIME
+);
+
+SHOW TABLES;
+SELECT COUNT(*) FROM sales_data;
+CREATE INDEX ts on sales_data(timestamp);
+SHOW INDEX FROM sales.sales_data;
